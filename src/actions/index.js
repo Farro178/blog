55ea4934bb993c5dee8f83eb261f5b0dcf1d94e3
Farro178 <jamesfarrelly178@gmail.com>
2021-07-31 00:02:44 +0200

    Introduced redux thunk to application to fix dispatch error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = () => {
+export const fetchPosts = () =>
   /* bad approach as it breaks the rules of redux
   const response = await jsonPlaceHolder.get("/posts");
   */
 
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
+  async (dispatch) => {
+    // inside this inner function there is no need to return an action.
+    const response = await jsonPlaceHolder.get("./posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
-};
