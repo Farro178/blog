7134c7d15e4d9793a11bdb53e7143840214e6551
Farro178 <jamesfarrelly178@gmail.com>
2021-08-01 01:04:50 +0200

    Added reducer so that api call now renders title and body

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,5 +9,5 @@ export const fetchPosts = () =>
     // inside this inner function there is no need to return an action.
     const response = await jsonPlaceHolder.get("./posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
   };
